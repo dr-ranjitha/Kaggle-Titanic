9decc85b73917f1f2261547c4806779f3563c14e
Ranjitha Dhanasekaran <51212846+dr-ranjitha@users.noreply.github.com>
2020-04-20 17:36:04 -0600

    Create titanic.py
    Importing libraries

---
+++ titanic.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 17:19:43 2020
+
+@author: drran
+"""
+# Importing the libraries
+# data analysis and wrangling
+import pandas as pd
+import numpy as np
+import random as rnd
+
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
